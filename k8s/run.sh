e93b78279719df6836dd5793b6c2006d85f3fe9d
Andrey Kolkov <andrey.kolkov@x5.ru>
2022-04-21 15:21:34 +0400

    added prometheus-operator and servie monitor manifest for our app

--- k8s/run.sh
+++ k8s/run.sh
@@ -1,8 +1,10 @@
 #! /usr/bin/env bash
 
 helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 
 helm install my-release bitnami/redis
+helm install prom prometheus-community/kube-prometheus-stack
 
 k apply -f ./app.yml
 
